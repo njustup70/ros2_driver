0858186e9274c98eaccb0342e5c3e7545e80290f
elaina <1463967532@qq.com>
2025-10-27 21:18:32 +0800

    化简rosbag_with_utils启动官方的rosbag 和干掉部分参数

--- src/rc_bringup/launch/rosbag_with_utils.launch.py
+++ src/rc_bringup/launch/rosbag_with_utils.launch.py
@@ -17,12 +17,8 @@ from launch.substitutions import Command, PathJoinSubstitution, FindExecutable
 from launch.conditions import LaunchConfigurationEquals
 def generate_launch_description():
     ld=LaunchDescription()
-    ld.add_action(DeclareLaunchArgument('use_rosbag_record', default_value='false', description='Record rosbag if use is True'))
-    ld.add_action(DeclareLaunchArgument('use_tf_publish',default_value='false',description='Publish tf tree if use is True'))
-    ld.add_action(DeclareLaunchArgument('use_fast_lio_tf',default_value='false',description='提供fast_lio的tf树'))
     ld.add_action(DeclareLaunchArgument('rate',default_value='1',description='rate of rosbag play'))
     ld.add_action(DeclareLaunchArgument('loop',default_value='false',description='loop of rosbag play'))
-    ld.add_action(DeclareLaunchArgument('image_topic',default_value='/camera/color/image_raw/compressed',description='image topic'))
     package_path=get_package_share_directory('rc_bringup')
     #启动rosbag
     rosbag_root_path='/home/Elaina/ros2_driver/bag_play'
@@ -50,15 +46,15 @@ def generate_launch_description():
         output='screen',
         condition=IfCondition(LaunchConfiguration('loop'))
     )
-    ros_bag_node=Node(
-        package='python_pkg',
-        executable='bag_play',
-        name='bag_play_node',
-        parameters=[{'loop': LaunchConfiguration('loop'),
-                     'rate': LaunchConfiguration('rate')}],
-        output='screen',
-        emulate_tty=True,
-    )
+    # ros_bag_node=Node(
+    #     package='python_pkg',
+    #     executable='bag_play',
+    #     name='bag_play_node',
+    #     parameters=[{'loop': LaunchConfiguration('loop'),
+    #                  'rate': LaunchConfiguration('rate')}],
+    #     output='screen',
+    #     emulate_tty=True,
+    # )
     #启动utils
     utils_launch=IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -66,27 +62,11 @@ def generate_launch_description():
         ),
         
     )
-    odomtransform_tf_node = Node(
-        condition=IfCondition(LaunchConfiguration('use_tf_publish')),
-        package='tf2_ros',
-        executable="static_transform_publisher",
-        name='odom_transform',
-        arguments=['0.35','7.65','0','0','0','0','odom','odom_transform']  # 发布静态变换
-    )
-    #启动image_bridge
-    image_bridge_node=Node(
-        package='python_pkg',
-        executable='image_bridge',
-        name='image_bridge_node',
-        parameters=[{'image_topic': LaunchConfiguration('image_topic')}],
-        output='screen',
-        emulate_tty=True,
-    )
     ld.add_action(utils_launch)
-    ld.add_action(ros_bag_node)
+    # ld.add_action(ros_bag_node)
     # ld.add_action(odomtransform_tf_node)
-    # ld.add_action(ros_bag_exe)
-    # ld.add_action(ros_bag_loop_exe)
+    ld.add_action(ros_bag_exe)
+    ld.add_action(ros_bag_loop_exe)
     # ld.add_action(image_bridge_node)
     return ld
      
